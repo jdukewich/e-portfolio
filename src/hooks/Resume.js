9d8fbd27b4eb2753d4af40d3eb1e4f2c053d06d8
jdukewich <jdukewich@gmail.com>
2020-05-03 19:30:18 -0400

    Working on mobile PDF views

--- src/hooks/Resume.js
+++ src/hooks/Resume.js
@@ -14,7 +14,9 @@ function Resume() {
                 My résumé contains a brief summary of some of my past work experiences as well as my technical skills. I have plenty of other projects and experiences, so check out other sections of my portfolio as well! If you have any questions about my experiences, I would be happy to answer them!              
                 </div>          
                 <div className="embed-responsive embed-responsive-16by9">
-                  <embed className="embed-responsive-item" src="/resume.pdf" />                  
+                	<object className="embed-responsive-item" data="resume.pdf">                		
+   					 				<p><a href="resume.pdf">Download PDF</a></p>
+                	</object>                               
                 </div>
             </div>
           </div>
